355661366a7f05f59ef076a6c0462b82f2ff603d
John Shelman <88163848+shelmanj@users.noreply.github.com>
2025-08-21 13:22:42 -0400

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,31 @@
+player_wins = 0
+computer_wins = 0
+import random
+while player_wins < 2 and computer_wins < 2:
+  print("Let's play rock, paper, or scissors!")
+  player_choice = input("Choose of rock, paper or scissors: ").lower()
+  choices = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(choices)
+  print(f"Computer chose: {computer_choice}")
+
+  if (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "rock"):
+    winner = "Player"
+  elif player_choice == computer_choice:
+    winner = "Tie"
+  else:
+    winner = "Computer"
+
+  if winner == "Player":
+    player_wins += 1
+    print("You won")
+  elif winner == "Computer":
+    computer_wins += 1
+    print("Computer won")
+  else:
+    print("It's a tie")
+else:
+  print (f"Current Score - Player: {player_wins}, Computer: {computer_wins}")
+  if (player_wins > computer_wins):
+    print ("Congratulations! You won.")
+  else:
+    print ("Computer won!")
